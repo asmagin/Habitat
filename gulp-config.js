d442559e6ab9df3a456d1be40d8080f2a14658fd
Alexander Smagin <alexander.v.smagin@gmail.com>
2017-10-04 11:15:36 -0400

    Change local configurations

--- gulp-config.js
+++ gulp-config.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-  var instanceRoot = "C:\\websites\\Habitat.dev.local";
+  var instanceRoot = "d:\\.projects\\Habitat\\sandbox";
   var config = {
     websiteRoot: instanceRoot + "\\Website",
     sitecoreLibraries: instanceRoot + "\\Website\\bin",
